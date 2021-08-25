0d59c5c5bdb31ec7e0d66eb56fdc37465759914c
huyongtao <2361622359@qq.com>
2021-08-25 13:06:39 +0800

    修改字体样式

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,27 @@ void main() => runApp(new MyApp()); // main函数使用了(=>)符号, 这是Dart
 class MyApp extends StatelessWidget { //该应用程序继承了 StatelessWidget，这将会使应用本身也成为一个widget。 在Flutter中，大多数东西都是widget(小部件)，包括对齐(alignment)、填充(padding)和布局(layout)
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+    // final wordPair = new WordPair.random();
     // 本示例创建一个Material APP。Material是一种标准的移动端和web端的视觉设计语言。 Flutter提供了一套丰富的Material widgets。
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold( // Scaffold 是 Material library 中提供的一个widget, 它提供了默认的导航栏、标题和包含主屏幕widget树的body属性。widget树可以很复杂。
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          // child: new Text('Hello World'),
-          child: new Text(wordPair.asPascalCase),
+        // appBar: new AppBar(
+        //   title: new Text('Welcome to Flutter'),
+        // ),
+        body: Center(
+          child: Text(
+            'Hello World,flutter的第一个组件，flutter一切皆组件，Material是一种标准的移动端和web端的视觉设计语言',
+            textAlign: TextAlign.center, // 设置文本对齐方式
+            maxLines: 2, // 设置最多显示的行数
+            style: TextStyle( 
+              fontSize: 25.0, // 字体大小为25.0
+              color: Color.fromARGB(255, 255, 150, 150), // 颜色为粉红色
+              decoration: TextDecoration.underline, // 有一个下划线
+              decorationStyle: TextDecorationStyle.solid, // 实线
+            ),
+            ),
+          // child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
